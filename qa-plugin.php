2bf884c1855b2acdfafa22837758bfb1bad91678
pupi1985 <pupi1985@bitbucket.org>
2019-02-02 00:48:57 -0300

    Reformat code

--- qa-plugin.php
+++ qa-plugin.php
@@ -14,32 +14,32 @@
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
-	header('Location: ../../');
-	exit;
+    header('Location: ../../');
+    exit;
 }
 
 qa_register_plugin_module(
-	'widget', // type of module
-	'qa-tag-desc-widget.php', // PHP file containing module class
-	'qa_tag_descriptions_widget', // module class name in that PHP file
-	'Tag Descriptions' // human-readable name of module
+    'widget', // type of module
+    'qa-tag-desc-widget.php', // PHP file containing module class
+    'qa_tag_descriptions_widget', // module class name in that PHP file
+    'Tag Descriptions' // human-readable name of module
 );
 
 qa_register_plugin_module(
-	'page', // type of module
-	'qa-tag-desc-edit.php', // PHP file containing module class
-	'qa_tag_descriptions_edit_page', // name of module class
-	'Tag Description Edit Page' // human-readable name of module
+    'page', // type of module
+    'qa-tag-desc-edit.php', // PHP file containing module class
+    'qa_tag_descriptions_edit_page', // name of module class
+    'Tag Description Edit Page' // human-readable name of module
 );
 
 qa_register_plugin_overrides('qa-tag-desc-overrides.php');
 
 qa_register_plugin_layer(
-	'qa-tag-desc-layer.php', // PHP file containing layer
-	'Tag Description Plugin Layer' // human-readable name of layer
+    'qa-tag-desc-layer.php', // PHP file containing layer
+    'Tag Description Plugin Layer' // human-readable name of layer
 );
 
 qa_register_plugin_phrases(
-	'qa-tag-desc-lang-*.php', // pattern for language files
-	'plugin_tag_desc' // prefix to retrieve phrases
+    'qa-tag-desc-lang-*.php', // pattern for language files
+    'plugin_tag_desc' // prefix to retrieve phrases
 );
